a6edbe99e31c28e138698d102fe1fa302b90eb27
Kai Volland <kaivolland@yahoo.de>
2019-02-01 10:11:39 +0100

    Introduces GraphQL parallel to rest

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
+const environment = process.env.NODE_ENV || 'production';
+const graphqlHTTP = require('express-graphql');
+
 // Setup Sequelize
 require('./sequelize.ts');
 
+// Setup GraphQL
+import graphqlSchema from './graphql';
+
 // Setup Express
-require('./express.ts');
+const app = require('./express.ts');
+
+app.use('/graphql', graphqlHTTP({
+  schema: graphqlSchema,
+  graphiql: environment === 'development'
+}));
